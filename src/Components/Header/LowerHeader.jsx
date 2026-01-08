4bb1927bd48edee88dad950ab4274a95136399f9
belaynehkassie01 <belaynehkassie01@gmail.com>
2026-01-08 06:44:16 -0800

    make the header responsive

--- src/Components/Header/LowerHeader.jsx
+++ src/Components/Header/LowerHeader.jsx
@@ -1,26 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './Header.module.css'
 import { IoMenuSharp } from "react-icons/io5";
-
+import { CgClose } from "react-icons/cg";
 
 function LowerHeader() {
-  return (
-    <div className={classes.lower_container}>
-      <ul>
-              
-        <li>
-          {/* menu icon */}
-          <IoMenuSharp />
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+  const menuItems = [
+    "Today's Deals",
+    "Customer Service", 
+    "Registry",
+    "Gift Cards",
+    "Sell"
+  ];
 
+  return (
+    <>
+      <div className={classes.lower_container}>
+        <ul>
+          <li onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <IoMenuSharp />
             <p>All</p>
-        </li>
-        <li> Today's Deals</li>
-        <li> Customer Service</li>
-        <li> Registry</li>
-        <li> Gift Cards</li>
-        <li> Sell</li>
-      </ul>
-    </div>
+          </li>
+          {menuItems.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+      
+      {/* Mobile Menu Overlay */}
+      {isMenuOpen && (
+        <div className={classes.mobile_menu_overlay}>
+          <div className={classes.mobile_menu}>
+            <div className={classes.mobile_menu_header}>
+              <h3>Menu</h3>
+              <button onClick={() => setIsMenuOpen(false)}>
+                <CgClose size={24} />
+              </button>
+            </div>
+            <ul>
+              <li>All</li>
+              {menuItems.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
